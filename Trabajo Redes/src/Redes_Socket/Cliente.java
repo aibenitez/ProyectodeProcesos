defc74bdb5bbaffcc10fb95ab7e8992423f57fa9
Andres Benitez <aibenitez@utpl.edu.ec>
2016-07-01 00:06:18 -0500

    Comentario

--- Trabajo Redes/src/Redes_Socket/Cliente.java
+++ Trabajo Redes/src/Redes_Socket/Cliente.java
@@ -14,7 +14,7 @@ import java.net.*;
 import java.io.*;
 
 public class Cliente extends Frame implements ActionListener, Runnable, KeyListener {
-
+//programa
     Socket soket;
     BufferedReader lectura;
     BufferedWriter escritura;
